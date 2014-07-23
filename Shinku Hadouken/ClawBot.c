8c1b250ea4543322da0729058761eddc92e71767
exploitativity <firerider521@gmail.com>
2014-07-23 10:13:25 -0700

    Updated Main to go back and forth.

--- Shinku Hadouken/ClawBot.c
+++ Shinku Hadouken/ClawBot.c
@@ -1,11 +1,20 @@
 int servo1port=2;
-int lowerServoPos=1756
-int upperServoPos=1531
+int lowerServoPos=1756;
+int upperServoPos=1531;
 int main()
 {
 	followLine();
+	mrp(1,500,-1400);
+	mrp(3,500,-1400);
+	msleep(3000);
+	
 	enable_servos();
 	set_servo_position(servo1port,lowerServoPos);
+	
+	mrp(1,500,-1400);
+	mrp(3,500,-1400);
+	
+	set_servo_position(servo1port,upperServoPos);
 	
 	return 0;
 }
