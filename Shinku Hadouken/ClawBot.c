7a08d601cc305ffa29d0f05e8d561974253d4b87
exploitativity <firerider521@gmail.com>
2014-07-23 09:55:45 -0700

    Changes to Main.

--- Shinku Hadouken/ClawBot.c
+++ Shinku Hadouken/ClawBot.c
@@ -1,8 +1,11 @@
+int servo1port=2;
+int lowerServoPos=1756
+int upperServoPos=1531
 int main()
 {
-	while(digital(10)==0)
-	{
-		followLine();
-	}
-	ao();
+	followLine();
+	enable_servos();
+	set_servo_position(servo1port,lowerServoPos);
+	
+	return 0;
 }
